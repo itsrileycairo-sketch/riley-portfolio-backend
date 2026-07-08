ba8b9f1e384fe7b4aa9a8c317a03e9b368fdb662
Riley <itsrileycairou@gmail.com>
2026-07-09 06:47:37 +0700

    Force PHP to display fatal errors for debugging

--- api/index.php
+++ api/index.php
@@ -1,4 +1,9 @@
 <?php
+// PAKSA PHP MENAMPILKAN FATAL ERROR MENTAH
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // BUKA PINTU CORS DARI GERBANG VERCEL
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
